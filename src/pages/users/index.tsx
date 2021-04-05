7c1f6fc91f4f9f1e1990c63f5eb40330d15e0170
Andre Souza <souza.andre1993@gmail.com>
2021-04-05 15:12:10 -0300

    completed UserList page

---
+++ src/pages/users/index.tsx
@@ -0,0 +1,113 @@
+import {
+  Flex,
+  Box,
+  Heading,
+  Button,
+  Icon,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Checkbox,
+  Tbody,
+  Td,
+  Text
+} from "@chakra-ui/react";
+import { RiAddLine } from "react-icons/ri";
+import { Header } from "../../components/Header";
+import { Pagination } from "../../components/Pagination";
+import { Sidebar } from "../../components/Sidebar";
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+
+      <Flex
+        w="100%"
+        my="6"
+        maxWidth={1480}
+        mx="auto"
+        px="6"
+      >
+        <Sidebar />
+
+        <Box flex="1" borderRadius="8" bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">Usuários</Heading>
+
+            <Button
+              as="a"
+              size="sm"
+              fontSize="small"
+              colorScheme="pink"
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar novo
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Usuário</Th>
+                <Th>Data de cadastro</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">André Souza</Text>
+                    <Text fontSize="small" color="gray.300">souza.andre1993@gmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>
+                  <Text>04 de abril de 2021</Text>
+                </Td>
+              </Tr>
+
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">André Souza</Text>
+                    <Text fontSize="small" color="gray.300">souza.andre1993@gmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>
+                  <Text>04 de abril de 2021</Text>
+                </Td>
+              </Tr>
+
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">André Souza</Text>
+                    <Text fontSize="small" color="gray.300">souza.andre1993@gmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>
+                  <Text>04 de abril de 2021</Text>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+
+          <Pagination />
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
