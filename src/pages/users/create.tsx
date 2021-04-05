cf6f74fc2e055f6c14540020e190219c737d0e59
Andre Souza <souza.andre1993@gmail.com>
2021-04-05 18:00:19 -0300

    refactored dashboar and user pages using responsive design

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -26,7 +26,7 @@ export default function UserList() {
       >
         <Sidebar />
 
-        <Box flex="1" borderRadius="8" bg="gray.800" p="8">
+        <Box flex="1" borderRadius="8" bg="gray.800" p={["6", "8"]}>
           <Heading size="lg" fontWeight="normal">
             Criar usuário
           </Heading>
@@ -34,12 +34,12 @@ export default function UserList() {
           <Divider my="6" borderColor="gray.700" />
 
           <VStack spacing="8">
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
               <Input name="name" label="Nome completo" />
               <Input name="email" label="Email" type="email" />
             </SimpleGrid>
 
-            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} w="100%">
               <Input name="password" label="Senha" type="password" />
               <Input name="password_confirmation" label="Confirmar senha" type="password" />
             </SimpleGrid>
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -11,7 +11,8 @@ import {
   Checkbox,
   Tbody,
   Td,
-  Text
+  Text,
+  useBreakpointValue
 } from "@chakra-ui/react";
 import { RiAddLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
@@ -19,6 +20,11 @@ import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const isWideScreen = useBreakpointValue({
+    base: false,
+    lg: true
+  })
+
   return (
     <Box>
       <Header />
@@ -32,7 +38,7 @@ export default function UserList() {
       >
         <Sidebar />
 
-        {/* <Box flex="1" borderRadius="8" bg="gray.800" p="8">
+        <Box flex="1" borderRadius="8" bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">Usuários</Heading>
 
@@ -50,16 +56,16 @@ export default function UserList() {
           <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
-                <Th px="6" color="gray.300" width="8">
+                <Th px={["4", "4", "6"]} color="gray.300" width="8">
                   <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Usuário</Th>
-                <Th>Data de cadastro</Th>
+                {isWideScreen && <Th>Data de cadastro</Th>}
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td px="6">
+                <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -68,45 +74,18 @@ export default function UserList() {
                     <Text fontSize="small" color="gray.300">souza.andre1993@gmail.com</Text>
                   </Box>
                 </Td>
-                <Td>
-                  <Text>04 de abril de 2021</Text>
-                </Td>
+                {isWideScreen && (
+                  <Td>
+                    <Text>04 de abril de 2021</Text>
+                  </Td>
+                )}
               </Tr>
 
-              <Tr>
-                <Td px="6">
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">André Souza</Text>
-                    <Text fontSize="small" color="gray.300">souza.andre1993@gmail.com</Text>
-                  </Box>
-                </Td>
-                <Td>
-                  <Text>04 de abril de 2021</Text>
-                </Td>
-              </Tr>
-
-              <Tr>
-                <Td px="6">
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold">André Souza</Text>
-                    <Text fontSize="small" color="gray.300">souza.andre1993@gmail.com</Text>
-                  </Box>
-                </Td>
-                <Td>
-                  <Text>04 de abril de 2021</Text>
-                </Td>
-              </Tr>
             </Tbody>
           </Table>
 
           <Pagination />
-        </Box> */}
+        </Box>
       </Flex>
     </Box>
   )
